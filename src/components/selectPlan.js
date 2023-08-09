1d7e2ad34cf0dbee0ec383f2b20aebe5dde615a7
Balogun Sodiq <sodiqb86@gmail.com>
2023-08-09 18:04:39 +0100

    full

--- src/components/selectPlan.js
+++ src/components/selectPlan.js
@@ -193,23 +193,6 @@ const SelectPlan = ({
           <div className="text-blue-950  font-bold">Yearly</div>
         </div>
       </div>
-
-      {/* <div className="flex flex-col sm:bg-white   lg:p-0 sm:mt-6 lg:mt-0 sm:full lg:w-full">
-        <div className="flex flex-row justify-between sm:mx-4 md:mx-12 lg:mx-20">
-          <button
-            onClick={backToStepOne}
-            className="md:text-sm lg:text-sm place-self-start bg-white mb-4 sm:mt-8 md:mt-6 lg:mt-20 text-gray-500 font-bold rounded-md"
-          >
-            Go back
-          </button>
-          <button
-            onClick={planHandler}
-            className="md:text-sm lg:text-sm place-self-end bg-blue-900 mb-4 sm:w-2/5 md:w-2/5 lg:w-1/4 sm:px-2 md:px-0 lg:px-2 py-3 sm:mt-4 lg:mt-20 text-white rounded-lg"
-          >
-            Next Step
-          </button>
-        </div>
-      </div> */}
       <Button back={backToStepOne} next={planHandler} />
     </div>
   );
--- src/components/summary.js
+++ src/components/summary.js
@@ -79,24 +79,6 @@ const Summary = ({
           </div>
         </div>
       </div>
-
-      {/* <div className="flex flex-col sm:bg-white lg:p-0 sm:mt-10 lg:mt-0 sm:full lg:w-full">
-        <div className="flex flex-row justify-between sm:mx-4 md:mx-12 lg:mx-20">
-          <button
-            onClick={backToStepThree}
-            className="md:text-sm lg:text-sm place-self-start bg-white mb-4 sm:mt-8 md:mt-6 lg:mt-20 text-gray-500 font-bold rounded-md"
-          >
-            Go back
-          </button>
-          <button
-            onClick={confirm}
-            className="md:text-sm lg:text-sm place-self-end bg-blue-900 mb-4 sm:w-2/5 md:w-2/5 lg:w-1/4 sm:px-2 md:px-0 lg:px-2 py-3 sm:mt-4 lg:mt-20 text-white rounded-lg"
-          >
-            Confirm
-          </button>
-        </div>
-      </div> */}
-
       <Button back={backToStepThree} next={confirm} />
     </div>
   );
