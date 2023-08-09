1d52e8779e3186f6b6d2ed99858481fecc0b46af
Balogun Sodiq <sodiqb86@gmail.com>
2023-08-09 17:02:47 +0100

    full

--- src/components/personalInfo.js
+++ src/components/personalInfo.js
@@ -3,7 +3,7 @@
 
 const PersonalInfo = ({stepOneColor, name,email,phone,nameError,emailError,phoneError,emailValid,phoneValid,infoHandler, nameHandler,emailHandler,phoneHandler}) => {
     return(
-        <div className={`${stepOneColor?'block':'hidden'} sm:-mt-10 md:-mt-0 lg:-mt-10 sm:relative`}>
+        <div className={`${stepOneColor?'block':'hidden'} sm:-mt-10 md:-mt-0 lg:-mt-10`}>
             <div className="sm:bg-white sm:rounded-lg sm:mx-6 sm:p-6 lg:p-12  ">
                     <p className='text-blue-950 sm:text-2xl lg:text-4xl lg:mt-2 font-bold'>Personal info</p>
                     <p className='text-gray-500  mt-4 text-sm'>Please provide your name, email address and phone number</p>
@@ -13,7 +13,7 @@ const PersonalInfo = ({stepOneColor, name,email,phone,nameError,emailError,phone
                             {nameError && (<label className='text-red-500 text-sm font-bold' >This field is required</label>)}
                             
                         </div>
-                        <input type='text' placeholder="e.g, John Doe" value={name} onChange={(e)=>nameHandler(e.target.value)} className={`h-12 border-2 border-gray-200 pl-4 rounded-lg hover:border-purple-700 hover:border focus:border  ${nameError?" placeholder-red-400":''}`}/>
+                        <input type='text' placeholder="e.g, John Doe" value={name} onChange={(e)=>nameHandler(e.target.value)} className={`appearance-none w-full h-12 border-2 border-gray-200 pl-4 rounded-lg hover:border-purple-700 hover:border focus:border  ${nameError?" placeholder-red-400":''}`}/>
                     </div>
                     <div className='flex flex-col mt-6'>
                     <div className='flex justify-between'>
@@ -21,7 +21,7 @@ const PersonalInfo = ({stepOneColor, name,email,phone,nameError,emailError,phone
                             {emailError && (<label className='text-red-500 text-sm font-bold' >This field is required</label>)}
                             {!emailValid && (<label className='text-red-500 text-sm font-bold' >Please enter a valid email address.</label>)}
                         </div>
-                        <input type='text' placeholder="e.g, test@gmail.com" value={email} onChange={(e)=>emailHandler(e.target.value)}  className={`h-12 border-2 border-gray-200 pl-4 rounded-lg hover:border-purple-700 hover:border focus:border ${emailError?" placeholder-red-400":''}`}/>
+                        <input type='text' placeholder="e.g, test@gmail.com" value={email} onChange={(e)=>emailHandler(e.target.value)}  className={` appearance-none w-full h-12 border-2 border-gray-200 pl-4 rounded-lg hover:border-purple-700 hover:border focus:border ${emailError?" placeholder-red-400":''}`}/>
                     </div>
                     <div className='flex flex-col mt-6'>
                     <div className='flex justify-between'>
@@ -29,7 +29,7 @@ const PersonalInfo = ({stepOneColor, name,email,phone,nameError,emailError,phone
                             {phoneError && (<label className='text-red-500 text-sm font-bold' >This field is required</label>)}
                             {!phoneValid && (<label className='text-red-500 text-sm font-bold' >phone number not valid</label>)}
                     </div>
-                        <input type='text' placeholder="e.g, +1 234 567 890" value={phone} onChange={(e)=>phoneHandler(e.target.value)} className={`h-12  border-2 border-gray-200 pl-4 rounded-lg hover:border-purple-700 hover:border focus:border ${phoneError?" placeholder-red-400":''}`}/>
+                        <input type='text' placeholder="e.g, +1 234 567 890" value={phone} onChange={(e)=>phoneHandler(e.target.value)} className={`appearance-none w-full h-12  border-2 border-gray-200 pl-4 rounded-lg hover:border-purple-700 hover:border focus:border ${phoneError?" placeholder-red-400":''}`}/>
                     </div>
                    
                     </div> 
